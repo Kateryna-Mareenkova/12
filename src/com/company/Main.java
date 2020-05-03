7e130f66d737cc0d172586aca764ae91e2886070
mareenkovakaterina@gmail.com <3313213704evb>
2020-05-03 10:29:08 +0300

    12

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args){
+    System.out.println();}
+
+
+    static int divides(int a=9, int b=25, int c=102){
+        if (a%3==0, b%3==0, c%3==0)
+    }
+}
